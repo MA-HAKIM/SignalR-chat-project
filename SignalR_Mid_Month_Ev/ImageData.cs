28042b6470f593eb43efd0b2cb571493b95d5013
ABDUL HAKIM <hakim124656@gmail.com>
2021-10-03 01:25:11 +0600

    SignalR chat project upload

---
+++ SignalR_Mid_Month_Ev/ImageData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Mid_Month_Ev
+{
+    public class ImageData
+    {
+        public string Filename { get; set; }
+        public string Image { get; set; }
+    }
+}
